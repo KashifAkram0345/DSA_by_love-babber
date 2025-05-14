010602bd841e6473534038dbeb04878b06898420
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-14 20:02:33 +0500

    typecasting

---
+++ 3typecasting.cpp
@@ -0,0 +1,67 @@
+ #include<iostream>
+ using namespace std;
+  int main(){
+ //unsigned ka matalab hai ky +ve value store hoti hai
+  // agar hum 
+  unsigned int a=5;
+ cout<<a<<endl;
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  // int a='a';
+  // cout<<a<<endl;
+  // char ch=98;
+  // cout<<ch<<endl;
+  
+ 
