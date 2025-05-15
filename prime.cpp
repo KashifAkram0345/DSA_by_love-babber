f2003956a0b0b46e13f6d63021fe4cb50fb8fa87
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-15 21:14:58 +0500

    print prime number

---
+++ prime.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int prime=0;
+//   int n;
+//   while(1){
+    
+  
+  
+//   cin>>n;
+//   int i=2;
+//   while(i<n){
+//     if(n%i==0){
+//       prime=1;
+//     }
+//     i=i+1;
+
+//   }
+//   if(prime==0){
+//     cout<<"prime number"<<endl;
+//   }
+//   else{
+//     cout<<"not aprime number"<<endl;
+//   }
+//   }
+// }
