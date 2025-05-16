96e06e7a06e822dc3152e833c1ab6d96f6cbdb60
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-16 16:35:31 +0500

    Using for loop

---
+++ for_loop12.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a value :"<<endl;
+    cin>>a;
+    cout<<"Printing value from 1-to-a :"<<endl;
+    for(int i=1;i<=a;i++){
+        cout<<i<<endl;
+    }
+}
