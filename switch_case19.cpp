6cb1475178acebba6547fd272a3dfd71e75fd1ae
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-18 14:12:37 +0500

    switch statement

---
+++ switch_case19.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x=1;
+   
+  switch(x){
+    case 1:
+    cout<<"one "<<endl;
+    break;
+  case 2:
+  cout<<"two"<<endl;
+  break;
+  case 3:
+  cout<<"three"<<endl;
+  break;
+  default:
+  cout<<"default"<<endl;
+  break;
+}
+ }
