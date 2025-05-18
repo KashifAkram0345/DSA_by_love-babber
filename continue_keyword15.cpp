a18936d7470b6334a0eb63b955d135ed51642f40
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-18 22:27:17 +0500

    continue keyword

---
+++ continue_keyword15.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=     5;i++){
+        cout<<"Assalam-o-Alaikom"<<endl;
+        cout<<"Hy"<<endl;
+        continue;
+        cout<<"Please reply"<<endl;
+    }
+}
