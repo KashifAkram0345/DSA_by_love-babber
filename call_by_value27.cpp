c4998df59486f171307e0fd00a9fdae73f4d9c4e
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-21 15:05:02 +0500

    call by value

---
+++ call_by_value27.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int value(int x){
+    cout<<"x is"<<x<<endl;
+}
+int main(){
+    int x;
+    cin>>x;
+    value(x);
+    cout<<"number is"<<x<<endl;
+
+
+}
