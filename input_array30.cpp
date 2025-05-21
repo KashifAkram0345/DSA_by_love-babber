53d936e7fae824d3d6adf08cc49d8813df97e508
KashifAkram0345 <m.kashif6833@gmail.com>
2025-05-21 21:38:59 +0500

    taking data from user

---
+++ input_array30.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x[10];
+    cin>>x[0];
+    cin>>x[1];
+      cin>>x[2];
+    cin>>x[3];
+    cin>>x[4];  
+    cin>>x[5];  
+    cin>>x[6];
+    
+      cin>>x[7];
+    cin>>x[8];
+      cin>>x[9];
+   cout<<"Ans is "<<x[0]<<endl;
+    cout<<"Ans is "<<x[1]<<endl;
+     cout<<"Ans is "<<x[2]<<endl;
+      cout<<"Ans is "<<x[3]<<endl;
+       cout<<"Ans is "<<x[4]<<endl;
+        cout<<"Ans is "<<x[5]<<endl;
+         cout<<"Ans is "<<x[6]<<endl;
+          cout<<"Ans is "<<x[7]<<endl;
+           cout<<"Ans is "<<x[8]<<endl;
+            cout<<"Ans is "<<x[9]<<endl;
+ int n[10];
+ for(int i=0;i<=9;i++){
+    cin>>n[i];
+
+ }
+ for(int i=0;i<=9;i++){
+ cout<<"second answer is"<<n[i]<<endl;
+ }
+
+    
+
+}
